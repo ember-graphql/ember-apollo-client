ef0d2eb04f3972ad1aa9ce0e9f52a2ebe2ef6bba
Blake Gentry <blakesgentry@gmail.com>
2017-01-09 08:31:43 -0800

    attempt to use direct imports of graphql deps
    Avoid the browserify / npm: prefixing. However, this fails because there
    are apparently multiple copies of graphql bundled, which breaks its
    internal type checking.

--- app/initializers/apollo.js
+++ app/initializers/apollo.js
@@ -1,10 +1,5 @@
 import config from '../config/environment';
 
-/* eslint-disable no-unused-vars */
-import graphql from 'npm:graphql';
-/* eslint-disable no-unused-vars */
-import graphqlTools from 'npm:graphql-tools';
-
 export function initialize() {
   const application = arguments[1] || arguments[0];
   const { apollo: { apiURL } } = config;
