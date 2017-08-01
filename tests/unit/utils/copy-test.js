92f9e00de4f21089df52ef4298c3088e72aa123d
Laurin Quast <laurinquast@googlemail.com>
2017-08-01 14:25:07 +0200

    Improves test security

--- tests/unit/utils/copy-test.js
+++ tests/unit/utils/copy-test.js
@@ -4,40 +4,80 @@ import { module, test } from 'qunit';
 module('Unit | Utility | copy');
 
 test('it can copy graphql properties that are prefixed with __', function (assert) {
-  const object = { __typename: 'Person' };
-  const copiedObject = copy(object);
-  assert.deepEqual(copiedObject, object)
+  const input = { 
+    __typename: 'Person',
+    name: 'Sven'
+  };
+  const expected = {
+    __typename: 'Person',
+    name: 'Sven'
+  }
+  const copiedObject = copy(input);
+  assert.deepEqual(copiedObject, expected)
 });
 
 test('it can copy graphql properties that are prefixed with __ in nested structures', function (assert) {
-  const object = {
+  const input = {
     __typename: 'Person',
+    age: 30,
     car: {
       __typename: 'Car',
       producer: {
-        __typename: 'Producer'
+        __typename: 'Producer',
+        country: 'DE'
+      }
+    }
+  }
+  const expected = {
+    __typename: 'Person',
+    age: 30,
+    car: {
+      __typename: 'Car',
+      producer: {
+        __typename: 'Producer',
+        country: 'DE'
       }
     }
   };
-  const copiedObject = copy(object);
-  assert.deepEqual(copiedObject, object);
+  const copiedObject = copy(input);
+  assert.deepEqual(copiedObject, expected);
 });
 
 test('it can copy graphql properties of objects with arrays', function (assert) {
-  const object = {
+  const input = {
     __typename: 'Person',
     cars: [
       {
         __typename: `Car`,
+        doorCount: 4,
       },
       {
         __typename: `Car`,
+        doorCount: 4,
       },
       {
         __typename: `Car`,
+        doorCount: 2,
       }
     ]
   };
-  const copiedObject = copy(object);
-  assert.deepEqual(copiedObject, object);
+  const expected = {
+    __typename: 'Person',
+    cars: [
+      {
+        __typename: `Car`,
+        doorCount: 4,
+      },
+      {
+        __typename: `Car`,
+        doorCount: 4,
+      },
+      {
+        __typename: `Car`,
+        doorCount: 2,
+      }
+    ]
+  };
+  const copiedObject = copy(input);
+  assert.deepEqual(copiedObject, expected);
 });
