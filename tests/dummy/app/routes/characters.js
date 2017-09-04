8a3c20d7f29272afdd1cf69863723ad2c4af6e49
Blake Gentry <blakesgentry@gmail.com>
2017-09-04 16:29:16 -0700

    don't explicitly inject apollo service into routes
    This is handled automatically now by the RouteQueryManager mixin.

--- tests/dummy/app/routes/characters.js
+++ tests/dummy/app/routes/characters.js
@@ -2,11 +2,7 @@ import Ember from 'ember';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import query from 'dummy/gql/queries/characters';
 
-const { inject: { service } } = Ember;
-
 export default Ember.Route.extend(RouteQueryManager, {
-  apollo: service(),
-
   queryParams: {
     kind: {
       refreshModel: true
--- tests/dummy/app/routes/luke.js
+++ tests/dummy/app/routes/luke.js
@@ -2,12 +2,9 @@ import Ember from 'ember';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import query from 'dummy/gql/queries/human';
 
-const { inject: { service } } = Ember;
-
 const variables = { id: '1000' };
 
 export default Ember.Route.extend(RouteQueryManager, {
-  apollo: service(),
   model() {
     return this.apollo.watchQuery({ query, variables }, 'human');
   },
