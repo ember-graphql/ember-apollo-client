156dd088cfe2fea4a8c552d64a9166e48d52830a
Michael Villander <nousvillander@gmail.com>
2018-01-21 17:03:16 -0200

    Removes middlewares

--- addon/utils/middlewares.js
+++
@@ -1,9 +0,0 @@
-import { computed } from "@ember/object";
-
-export default function middlewares(...middlewares) {
-  return computed(function() {
-    return middlewares.map((middleware) => {
-      return { applyMiddleware: this.get(middleware).bind(this) };
-    });
-  });
-}
