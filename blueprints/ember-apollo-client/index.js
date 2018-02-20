01c4090c49900ef8f062808083425104906f271e
Chris Ball <chris@echobind.com>
2018-02-19 22:31:41 -0500

    Fixes an error when running default blueprint
    Without this change, installation of the adddon fails with "the `ember generate <entity-name>` command requires an entity name to be specified."
    
    To fix it, we add a `normalizeEntityName` function to the blueprint.

--- blueprints/ember-apollo-client/index.js
+++ blueprints/ember-apollo-client/index.js
@@ -2,13 +2,15 @@
 module.exports = {
   description: 'Add ember-fetch in the consumer app',
 
+  normalizeEntityName() {},
+
   /*
   * This is necessary until this is fixed
   * https://github.com/ember-cli/ember-fetch/issues/98
   */
   afterInstall() {
     return this.addPackagesToProject([
-      {name: 'ember-fetch', target: '^3.4.4'}
+      { name: 'ember-fetch', target: '^3.4.4' }
     ]);
   }
 };
