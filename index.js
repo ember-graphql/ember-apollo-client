faf08dddc2fe4895a5c692a5b35f669ffab87314
zigahertz <zach@flood.io>
2018-03-12 13:43:24 -0700

    fix semicolon

--- index.js
+++ index.js
@@ -10,24 +10,24 @@ const defaultPackages = [
   'graphql',
   'graphql-tools',
   'graphql-tag',
-]
+];
 
 module.exports = {
   name: 'ember-apollo-client',
 
   included(app) {
-    this._super.included.apply(this, arguments)
+    this._super.included.apply(this, arguments);
     
-    let config = app.project.config(app.env) || {}
-    this.addonConfig = config['apollo'] || {}
+    let config = app.project.config(app.env) || {};
+    this.addonConfig = config['apollo'] || {};
     
-    this.import('vendor/-apollo-client-bundle.js')
-    this.import('vendor/-apollo-client-shims.js')
+    this.import('vendor/-apollo-client-bundle.js');
+    this.import('vendor/-apollo-client-shims.js');
   },
   
   treeForVendor() {
-    const WebpackDependencyPlugin = require('./lib/webpack-dependency-plugin')
-    const userSpecifiedPackages = this.addonConfig.include
+    const WebpackDependencyPlugin = require('./lib/webpack-dependency-plugin');
+    const userSpecifiedPackages = this.addonConfig.include;
     
     return new WebpackDependencyPlugin({
       outputName: 'apollo-client',
@@ -41,10 +41,10 @@ module.exports = {
         name: 'ember-apollo-client',
         ext: 'graphql',
         toTree(tree) {
-          const GraphQLFilter = require('./lib/graphql-filter')
-          return new GraphQLFilter(tree)
-        },
-      })
+          const GraphQLFilter = require('./lib/graphql-filter');
+          return new GraphQLFilter(tree);
+        }
+      });
     }
-  },
-}
+  }
+};
