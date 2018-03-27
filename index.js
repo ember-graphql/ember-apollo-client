7ebe28dfdd2c2c484e4d45715f32dfc1fe9c6c19
zigahertz <zach@flood.io>
2018-03-27 11:14:47 -0700

    add ability to exclude packages; incorporate feedback from PR

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
-'use strict'
+"use strict";
 
-const defaultPackages = [
-  'apollo-cache',
-  'apollo-cache-inmemory',
-  'apollo-client',
-  'apollo-link',
-  'apollo-link-context',
-  'apollo-link-http',
-  'graphql',
-  'graphql-tools',
-  'graphql-tag',
+const apolloClientDefaultPackages = [
+  "apollo-cache",
+  "apollo-cache-inmemory",
+  "apollo-client",
+  "apollo-link",
+  "apollo-link-context",
+  "apollo-link-http",
+  "graphql",
+  "graphql-tools",
+  "graphql-tag"
 ];
 
 module.exports = {
-  name: 'ember-apollo-client',
+  name: "ember-apollo-client",
 
-  included(app) {
+  included({ project, env }) {
     this._super.included.apply(this, arguments);
-    
-    let config = app.project.config(app.env) || {};
-    this.addonConfig = config['apollo'] || {};
-    
-    this.import('vendor/-apollo-client-bundle.js');
-    this.import('vendor/-apollo-client-shims.js');
+    this.addonConfig = project.config(env)["apollo"] || {};
+
+    this.import("vendor/-apollo-client-bundle.js");
+    this.import("vendor/-apollo-client-shims.js");
   },
-  
+
   treeForVendor() {
-    const WebpackDependencyPlugin = require('./lib/webpack-dependency-plugin');
-    const userSpecifiedPackages = this.addonConfig.include;
-    
+    const WebpackDependencyPlugin = require("./lib/webpack-dependency-plugin");
+    const {
+      addonConfig: { include: userPackages, exclude: excludedPackages }
+    } = this;
+    const includedPackages = apolloClientDefaultPackages.filter(
+      p => !excludedPackages.includes(p)
+    );
+
     return new WebpackDependencyPlugin({
-      outputName: 'apollo-client',
-      expose: [...defaultPackages, ...userSpecifiedPackages],
-    })
+      outputName: "apollo-client",
+      expose: [...includedPackages, ...userPackages]
+    });
   },
-  
+
   setupPreprocessorRegistry(type, registry) {
-    if (type === 'parent') {
-      registry.add('js', {
-        name: 'ember-apollo-client',
-        ext: 'graphql',
+    if (type === "parent") {
+      registry.add("js", {
+        name: "ember-apollo-client",
+        ext: "graphql",
         toTree(tree) {
-          const GraphQLFilter = require('./lib/graphql-filter');
+          const GraphQLFilter = require("./lib/graphql-filter");
           return new GraphQLFilter(tree);
         }
       });
