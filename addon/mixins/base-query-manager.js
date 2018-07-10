ce16c7e124cf73b45a48ee46ab06739a8263e1c9
Mike North <michael.l.north@gmail.com>
2018-07-10 16:37:25 -0700

    Replace 0-arg CP w/ eager instantiation (#146)

--- addon/mixins/base-query-manager.js
+++ addon/mixins/base-query-manager.js
@@ -1,11 +1,10 @@
 import { inject as service } from '@ember/service';
 import Mixin from '@ember/object/mixin';
-import { computed } from '@ember/object';
 
 export default Mixin.create({
   apolloService: service('apollo'),
-
-  apollo: computed(function() {
-    return this.get('apolloService').createQueryManager();
-  }),
+  init() {
+    this._super(...arguments);
+    this.set('apollo', this.get('apolloService').createQueryManager());
+  },
 });
