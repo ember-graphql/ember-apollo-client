20364f8714753bbfb85c72a7d074a8a628d1e947
Jan Buschtöns <jan@buschtoens.me>
2018-12-17 16:09:43 +0100

    fix(services/apollo): pass args for init

---
+++ types/@ember/object/core.d.ts
@@ -0,0 +1,7 @@
+import _CoreObject from 'ember__object/core';
+export * from 'ember__object/core';
+
+export default class CoreObject extends _CoreObject {
+  // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/31481
+  init(...args: any): void;
+}
