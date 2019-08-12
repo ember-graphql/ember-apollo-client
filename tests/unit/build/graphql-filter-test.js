73cffaa4b18333e8d39b39691c6397179d2c7146
Killa <lukasz.strzebinczyk@gmail.com>
2019-08-12 13:00:45 +0200

    find a failing test case

--- tests/unit/build/graphql-filter-test.js
+++ tests/unit/build/graphql-filter-test.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 
 import testFragment from './test-fragment';
 import testQuery from './test-query';
+import testQueryWithNestedFragments from './test-query-with-nested-fragment';
 import testSubscription from './test-subscription';
 
 module('Unit | graphql-filter', function() {
@@ -42,6 +43,26 @@ module('Unit | graphql-filter', function() {
         subject {
           ...testFragment
         }
+      }
+
+      fragment testFragment on Object {
+        name
+      }
+    `,
+  });
+
+  testCompilation('compilation with nested fragments', {
+    actual: testQueryWithNestedFragments,
+    expected: gql`
+      query TestQueryWithNestedFragment {
+        subject {
+          ...testFragmentWithNestedFragment
+          ...testFragment
+        }
+      }
+
+      fragment testFragmentWithFragment on Object {
+        ...testFragment
       }
 
       fragment testFragment on Object {
