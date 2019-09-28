28f622b6febe2f93b6b453200eb96abb3959b72f
Josemar Luedke <josemarluedke@gmail.com>
2019-09-28 14:06:05 -0700

    Allow pretender.js to run in dev mode

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,11 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {});
+  let app = new EmberAddon(defaults, {
+    pretender: {
+      enabled: true,
+    },
+  });
 
   /*
     This build file specifies the options for the dummy test app of this
