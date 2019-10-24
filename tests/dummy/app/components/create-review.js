9b503ca0d2dff9735307f4cd4d9d4b2f977db200
Josemar Luedke <josemarluedke@gmail.com>
2019-10-24 11:33:44 -0700

    Refactor mutation tests to acceptance instead of unit tests

---
+++ tests/dummy/app/components/create-review.js
@@ -0,0 +1,33 @@
+import Component from '@ember/component';
+import layout from '../templates/components/create-review';
+import mutation from 'dummy/gql/mutations/create-review';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  layout,
+
+  init() {
+    this._super(...arguments);
+
+    this.review = {};
+    this.response = null;
+  },
+
+  apollo: service(),
+
+  actions: {
+    createReview(movieId, stars, commentary) {
+      let variables = {
+        movieId,
+        review: {
+          stars: parseInt(stars),
+          commentary,
+        },
+      };
+
+      return this.apollo.mutate({ mutation, variables }).then(response => {
+        this.set('response', JSON.stringify(response));
+      });
+    },
+  },
+});
--- tests/dummy/app/routes/new-review.js
+++ tests/dummy/app/routes/new-review.js
@@ -1,19 +1,3 @@
 import Route from '@ember/routing/route';
-import EmberObject from '@ember/object';
-import mutation from 'dummy/gql/mutations/create-review';
-import { inject as service } from '@ember/service';
 
-export default Route.extend({
-  apollo: service(),
-
-  model() {
-    return EmberObject.create({});
-  },
-
-  actions: {
-    createReview(movieId, review) {
-      let variables = { movieId, review };
-      return this.apollo.mutate({ mutation, variables }, 'review');
-    },
-  },
-});
+export default Route.extend({});
