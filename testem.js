0b2171b89a660c08a9052dc2df5a61008a7c910c
Josemar Luedke <josemarluedke@gmail.com>
2019-12-10 17:19:17 -0800

    Run tests in GitHub Actions

--- testem.js
+++ testem.js
@@ -1,20 +1,28 @@
+const CHROME_ARGS = [
+  // --no-sandbox is needed when running Chrome inside a container
+  process.env.CI ? '--no-sandbox' : null,
+  '--headless',
+  '--disable-dev-shm-usage',
+  '--disable-software-rasterizer',
+  '--mute-audio',
+  '--remote-debugging-port=0',
+  '--window-size=1440,900',
+].filter(Boolean);
+
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
+  launch_in_ci: ['Chromium', 'Chrome'],
+  launch_in_dev: ['Chromium'],
+  ignore_missing_launchers: true,
   browser_args: {
+    Chromium: {
+      ci: CHROME_ARGS,
+      dev: CHROME_ARGS,
+    },
     Chrome: {
-      ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
-        '--headless',
-        '--disable-dev-shm-usage',
-        '--disable-software-rasterizer',
-        '--mute-audio',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900',
-      ].filter(Boolean),
+      ci: CHROME_ARGS,
+      dev: CHROME_ARGS,
     },
   },
 };
