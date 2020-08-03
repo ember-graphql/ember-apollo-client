4d7e322f86910c1882adac31d9b6d7a12c11ab86
Josemar Luedke <josemarluedke@gmail.com>
2020-08-02 17:31:38 -0700

    Remove deprecated mixins

--- addon/index.js
+++ addon/index.js
@@ -1,6 +1,3 @@
-export { default as ComponentQueryManager } from './-private/mixins/component-query-manager';
-export { default as ObjectQueryManager } from './-private/mixins/object-query-manager';
-export { default as RouteQueryManager } from './-private/mixins/route-query-manager';
 export {
   default as QueryManager,
   queryManager,
